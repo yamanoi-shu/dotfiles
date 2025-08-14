37aa865bb221fd1d7ac41eb1d0774261348bcbd3
yamanoi-shu <yamanoi_shu@cyberagent.co.jp>
2025-08-14 19:11:32 +0900

    Add oh-my-fish

---
+++ .config/fish/conf.d/bobthefish.fish
@@ -0,0 +1,7 @@
+set -g theme_color_scheme gruvbox
+set -g theme_display_git_master_branch yes
+set -g theme_display_date no
+set -g theme_nerd_fonts yes
+set -g theme_newline_cursor yes
+set -g theme_display_go no
+set -g fish_prompt_pwd_dir_length 10
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,3 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-
-    # PATH
-    set PATH /opt/homebrew/bin $PATH # <-追加
-end
-
-set -x GOOGLE_APPLICATION_CREDENTIALS "/Users/s17573/creds/love-221800-112db2d2d2d6.json"
-set -x FIREBASE_CREDENTIALS "/Users/s17573/creds/love-dev-firebase-7b11d79e9697.json"
-set -x SPANNER_INSTANCE_ID love-local
-set -x SPANNER_EMULATOR_HOST localhost:9010
-set -x PUBSUB_EMULATOR_HOST "localhost:9000"
-set -x IGNORE_RACE true
-set -x BUILD_ENV darwin-arm64
-
 alias gs='git status'
 alias gb='git branch'
 alias gc='git checkout'
@@ -25,12 +6,15 @@ alias vi="nvim"
 alias g='cd $(ghq root)/$(ghq list | peco)'
 alias "gsf"="git branch -a | fzf | sed 's/remotes\/origin\///g' | xargs git switch"
 alias ku="kubectl"
+alias ls="ls --color=auto -l"
+alias :q="exit"
 
 # go
 set -x PATH $PATH /usr/local/go/bin
 set -x GOROOT /usr/local/go
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
+set -x PATH $PATH /usr/local/bin
 
 # uv
 set -x PATH $PATH $HOME/.local/bin
